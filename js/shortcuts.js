970194a9f13509aee85b0ca4e5da213a9112f8f2
Rishabh Gusain <r.gausai@gmail.com>
2020-10-06 10:27:32 +0530

    use ES6 specs

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -1,7 +1,8 @@
+"use strict";
 document.addEventListener('keydown', function (event) {
     if (event.keyCode === 86 && event.ctrlKey && event.shiftKey) {
         setTimeout(function () {
-            sourceCode = event.target.innerText;
+            let sourceCode = event.target.innerText;
             sourceCode = "```\n" + sourceCode + "\n```";
             event.target.innerHTML = sourceCode;
         }, 100);
